692ccf2c8399bbd09483e5f5f0d48f6fafb6397b
Hashir Shahzad <hashir447079@gmail.com>
2025-07-12 13:23:35 +0500

    Still working

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
 use keyring::Entry;
 use matrix_sdk::{
     Client,
     config::SyncSettings,
-    ruma::{events::room::message::SyncRoomMessageEvent, user_id},
+    ruma::{UserId, events::room::message::SyncRoomMessageEvent, user_id},
 };
 
+use whoami;
+
+use std::fs::File;
+use std::path::Path;
+
+use colored::Colorize;
+#[cfg(windows)]
+use colored::control;
+#[cfg(windows)]
+use crossterm;
+
 struct MatrixLoginInfo {
-    user_id :,
-    password : str
+    username: &'static str,
+    password: &'static str,
 }
 
+macro_rules! logln {
+    ($($arg:tt)*) => {
+        println!(
+            "{} {}",
+            "[Main]".bold().green(),
+            format!($($arg)*)
+        );
+    };
+}
 
+static SERVICE_NAME: &str = "rust-matrix";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let service = "rust-matrix";
+    enable_ansi_support();
 
+    let login_info = get_user_login_info();
     let user_id = user_id!("@bongopoyo:matrix.org");
     let client = Client::builder()
         .server_name(user_id.server_name())
@@ -25,12 +49,12 @@ async fn main() -> anyhow::Result<()> {
     // First we need to log in.
     client
         .matrix_auth()
-        .login_username(user_id, "2zgIThXpip6WX4")
+        .login_username(user_id, login_info.password)
         .send()
         .await?;
 
     client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
-        println!("Received a message {:?}", ev);
+        logln!("Received a message {:#?}", ev);
     });
 
     // Syncing is important to synchronize the client state with the server.
@@ -40,6 +64,43 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+fn get_user_login_info() -> MatrixLoginInfo {
+    let mut matrix_login_info = MatrixLoginInfo {
+        username: "",
+        password: "",
+    };
+    let username = whoami::username();
+    logln!("Current user: {}", username);
+    let entry = Entry::new(SERVICE_NAME, username.as_str());
 
-fn get_user_info() {
+    match entry {
+        Ok(entry) => {
+            logln!("Entry exists: {:?}", entry);
+            match entry.get_password() {
+                Ok(password) => {
+                    logln!("Password: {}", password);
+                }
+                Err(e) => {
+                    logln!("Error getting password: {}", e);
+                }
+            }
+        }
+        Err(e) => {
+            logln!("Error getting entry: {}", e);
+        }
+    }
+
+    matrix_login_info
+}
+
+#[cfg(windows)]
+fn enable_ansi_support() {
+    println!("[Main] Detected Windows.... Enabling ANSI SUPPORT for colors...");
+    control::set_virtual_terminal(true).unwrap();
+    // crossterm::terminal::enable_virtual_terminal_processing(std::io::stdout()).unwrap();
+}
+
+#[cfg(not(windows))]
+fn enable_ansi_support() {
+    logln!("Detected UNIX BASED OS....");
 }
